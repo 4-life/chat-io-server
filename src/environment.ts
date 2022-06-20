f171158705655f2fffd7a0e059707db4e2fd4c9f
4life <povchinnikov@nwave.io>
2022-06-20 12:10:41 +0300

    Fix cors

--- src/environment.ts
+++ src/environment.ts
@@ -1,7 +1,7 @@
 export default function env() {
   if (process.env.NODE_ENV === 'production') {
     return {
-      CLIENT_URL: 'https://test.4life.work',
+      CLIENT_URL: 'https://chat-io-server-4life.herokuapp.com',
     };
   }
 
